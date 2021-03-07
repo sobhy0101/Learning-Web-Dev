a165cc65bf3629da6c3fa5e6dbaf7a437b17bdb6
Mike Sobhy <38540947+sobhy0101@users.noreply.github.com>
2021-03-07 16:44:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var grade = 70;
+var grade = 55
 
 if(grade > 90 && grade <= 100){
-    console.log('Excellent');
+    console.log('Excellent')
 }
 else if(grade > 80 && grade <= 90){
-    console.log('Very Good');
+    console.log('Very Good')
 }
 else if(grade > 70 && grade <= 80){
-    console.log('Good');
+    console.log('Good')
 }
-else if(grade > 60 && grade <= 50){
-    console.log('Accepted');
+else if(grade > 60 && grade <= 70){
+    console.log('Accepted')
 }
 else {
-    console.log('You have failed');
+    console.log('You have failed')
 };
